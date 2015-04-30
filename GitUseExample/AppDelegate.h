6718f55e231af152cb63b732859d1befdace056e
Ray <rayleeriver@gmail.com>
2015-04-30 11:38:12 -0700

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Ray Lee on 4/30/15.
+//  Copyright (c) 2015 SWP Biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Ray Lee on 4/30/15.
+//  Copyright (c) 2015 SWP Biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
